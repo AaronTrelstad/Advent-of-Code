81cb4051a39e15128bae3df72545d74da253089f
AaronTrelstad <trelstadaaron@gmail.com>
2025-01-17 18:47:31 -0600

    day 3

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,16 +2,21 @@
 #include <fstream>
 #include <cctype>
 
-inline int parse_num(std::string str, int length, int* indexPtr) {
+inline int parse_num(std::string str, int length, int *indexPtr)
+{
     int left = 0;
     int i = *indexPtr;
 
-    while (i < length) {
+    while (i < length)
+    {
         char character = str[i];
 
-        if (character == ',') {
+        if (character == ',')
+        {
             break;
-        } else if (character < '0' || character > '9') {
+        }
+        else if (character < '0' || character > '9')
+        {
             *indexPtr = i;
             return 0;
         }
@@ -20,19 +25,24 @@ inline int parse_num(std::string str, int length, int* indexPtr) {
         i++;
     }
 
-    if (i == length) {
+    if (i == length)
+    {
         return 0;
     }
 
     i++;
     int right = 0;
 
-    while (i < length) {
+    while (i < length)
+    {
         char character = str[i];
 
-        if (character == ')') {
+        if (character == ')')
+        {
             break;
-        } else if (character < '0' || character > '9') {
+        }
+        else if (character < '0' || character > '9')
+        {
             *indexPtr = i;
             return 0;
         }
@@ -41,7 +51,8 @@ inline int parse_num(std::string str, int length, int* indexPtr) {
         i++;
     }
 
-    if (i == length) {
+    if (i == length)
+    {
         *indexPtr = i;
         return 0;
     }
@@ -50,35 +61,108 @@ inline int parse_num(std::string str, int length, int* indexPtr) {
     return left * right;
 }
 
-int main() {
+int main()
+{
     std::ifstream inputFile("input.txt");
     std::string str;
 
-    if (inputFile.is_open()) {
+    if (inputFile.is_open())
+    {
         std::string line;
 
-        while (std::getline(inputFile, line)) {
+        while (std::getline(inputFile, line))
+        {
             str += line;
         }
     }
 
+    // Part 1
     char mul_chars[] = {'m', 'u', 'l', '('};
-    int place = 0;
-    int num_length = 0;
+
+    int mul_place = 0;
     int ans = 0;
 
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; i < str.length(); i++)
+    {
         char character = str[i];
 
-        if (place != 4) {
-            if (character == mul_chars[place]) {
-                place++;
-            } else {
-                place = 0;
+        if (mul_place != 4)
+        {
+            if (character == mul_chars[mul_place])
+            {
+                mul_place++;
             }
-        } else {
+            else
+            {
+                mul_place = 0;
+            }
+        }
+        else
+        {
             ans += parse_num(str, str.length(), &i);
-            place = 0;
+            mul_place = 0;
+        }
+    }
+
+    std::cout << ans << std::endl;;
+
+    // Part 2
+    char do_chars[] = {'d', 'o', '(', ')'};
+    char dont_chars[] = {'d', 'o', 'n', '\'', 't', '(', ')'};
+
+    mul_place = 0;
+    int do_place = 0;
+    int dont_place = 0;
+    ans = 0;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        char character = str[i];
+
+        if (dont_place != 7)
+        {
+            if (character == dont_chars[dont_place])
+            {
+                dont_place++;
+            }
+            else
+            {
+                dont_place = 0;
+
+                if (mul_place != 4)
+                {
+                    if (character == mul_chars[mul_place])
+                    {
+                        mul_place++;
+                    }
+                    else
+                    {
+                        mul_place = 0;
+                    }
+                }
+                else
+                {
+                    ans += parse_num(str, str.length(), &i);
+                    mul_place = 0;
+                }
+            }
+        }
+        else
+        {
+            if (character == do_chars[do_place])
+            {
+                do_place++;
+
+                if (do_place == 4)
+                {
+                    do_place = 0;
+                    dont_place = 0;
+                }
+            }
+            else
+            {
+                do_place = 0;
+            }
         }
     }
 
---
+++ 4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream inputFile("input.txt");
+
+    if (inputFile.is_open()) {
+        
+    }
+}
